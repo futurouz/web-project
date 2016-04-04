04c4234e27ca678569ec332bff60863df04a140f
Kunanan <supercell396@gmail.com>
2016-04-04 21:47:20 +0700

    COMING SOON

--- js/app.js
+++ js/app.js
@@ -1,9 +1,13 @@
-$(document).ready(function(){
+$(document).ready(function(){	
     $('.bg h1').click(function(){
         $('.bg').animate({left: "-350px"},250);
     });
     $('.menu h2').click(function(){
         $('.bg').animate({left: "0px"},250);
+    });
+    $('.bg p').click(function(){
+    	$(this).fadeOut(200);
+    	$('#cs').fadeIn(1000);
     });
     
 });
