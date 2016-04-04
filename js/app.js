ea5c1445c49a61bbe7edc38a25b8fe5c00ea3220
Kunanan <supercell396@gmail.com>
2016-04-04 16:53:47 +0700

    First Page

--- js/app.js
+++ js/app.js
@@ -1,9 +1,9 @@
 $(document).ready(function(){
     $('.bg h1').click(function(){
-        $('.bg').animate({left: "-285px"},400);
+        $('.bg').animate({left: "-350px"},250);
     });
     $('.menu h2').click(function(){
-        $('.bg').animate({left: "0px"},400);
+        $('.bg').animate({left: "0px"},250);
     });
     
 });
