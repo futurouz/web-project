2825320d2076bd3d762f35e716f833bf7995092e
Kunanan <supercell396@gmail.com>
2016-04-05 01:05:01 +0700

    Add Shop page

--- js/app.js
+++ js/app.js
@@ -2,6 +2,7 @@ $(document).ready(function(){
     $('.bg h1').click(function(){
         $('.bg').animate({left: "-350px"},250);
     });
+
     $('.menu h2').click(function(){
         $('.bg').animate({left: "0px"},250);
     });
@@ -9,5 +10,14 @@ $(document).ready(function(){
     	$(this).fadeOut(200);
     	$('#cs').fadeIn(1000);
     });
-    
+
+
+
+    $('.fixed-nav-bar h2').click(function(){
+        $('.content ').animate({left: "-350px"},250);
+        $('.fixed-nav-bar').animate({left: "-350px"},250);
+    });
+    $('.content h2').click(function(){
+        $('.content').animate({left: "0px"},250);
+    });
 });
