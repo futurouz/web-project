9ef2bb62da1118e7c0253509198f83e093b00397
Kunanan <supercell396@gmail.com>
2016-04-09 12:21:28 +0700

    p2 and p3

--- js/app.js
+++ js/app.js
@@ -17,11 +17,13 @@ $(document).ready(function(){
         $('.menu').animate({right: "0px"},250);
         $('.fixed-nav-bar').animate({left: "-350px"},250);
         $('.allpic').animate({left: "-350px"},250);
+        $('.d').animate({left: "-350px"},250);
     });
     $('.menu h2').click(function(){
         $('.menu').animate({right: "-350px"},250);
         $('.fixed-nav-bar').animate({left: "0px"},250);
         $('.allpic').animate({left: "0px"},250);
+         $('.d').animate({left: "0px"},250);
     });
 
 });
