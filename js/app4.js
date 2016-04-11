4f9c796895b4e07a8cb8ab29bbf3c226d531b6de
Kunanan <supercell396@gmail.com>
2016-04-11 14:02:52 +0700

    Add about page

---
+++ js/app4.js
@@ -0,0 +1,8 @@
+function Show() {
+	document.getElementById("mem").style.visibility = "visible";
+	document.getElementById("mem").style.opacity = 0.95;
+}
+function Hide(){
+	document.getElementById("mem").style.visibility = "hidden";
+	document.getElementById("mem").style.opacity = 0;
+}
