907def64c8a97bd0c42a77896c37da08bd92dd56
Kunanan <supercell396@gmail.com>
2016-05-04 23:05:24 +0700

    add login panel

--- js/app.js
+++ js/app.js
@@ -1,3 +1,11 @@
+function showlogin() {
+	document.getElementById("login").style.visibility = "visible";
+	document.getElementById("login").style.opacity = 1;
+}
+function closelogin() {
+	document.getElementById("login").style.visibility = "hidden";
+	document.getElementById("login").style.opacity = 0;
+}
 function Show() {
 	document.getElementById("mem").style.visibility = "visible";
 	document.getElementById("mem").style.opacity = 0.95;
@@ -10,5 +18,7 @@ window.onload = function(){
 	setTimeout(function(){
 		document.getElementById("abouteat").style.opacity = 1;
 		document.getElementById("bt").style.opacity = 1;
+		document.getElementById("bt2").style.opacity = 1;
+		document.getElementById("bt3").style.opacity = 1;
 	},500);
 }
