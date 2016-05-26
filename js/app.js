e148373f41f5e16f6daaf8718a18cd9c88e941ee
Kunanan Tassuwan <Kunanan Tassuwan>
2016-05-26 23:34:45 +0700

    CompleteWeb lvl1

--- js/app.js
+++ js/app.js
@@ -1,10 +1,28 @@
+function showReg(){
+	document.getElementById("reg").style.visibility = "visible";
+	document.getElementById("reg").style.opacity = 1;
+	document.getElementById("fade").style.visibility = "visible";
+	document.getElementById("fade").style.opacity = 0.7;
+}
+
+function closeReg() {
+	document.getElementById("reg").style.visibility = "hidden";
+	document.getElementById("reg").style.opacity = 0;
+	document.getElementById("fade").style.visibility = "hidden";
+	document.getElementById("fade").style.opacity = 0;
+}
+
 function showlogin() {
 	document.getElementById("login").style.visibility = "visible";
 	document.getElementById("login").style.opacity = 1;
+	document.getElementById("fade").style.visibility = "visible";
+	document.getElementById("fade").style.opacity = 0.7;
 }
 function closelogin() {
 	document.getElementById("login").style.visibility = "hidden";
 	document.getElementById("login").style.opacity = 0;
+	document.getElementById("fade").style.visibility = "hidden";
+	document.getElementById("fade").style.opacity = 0;
 }
 function Show() {
 	document.getElementById("mem").style.visibility = "visible";
@@ -18,7 +36,5 @@ window.onload = function(){
 	setTimeout(function(){
 		document.getElementById("abouteat").style.opacity = 1;
 		document.getElementById("bt").style.opacity = 1;
-		document.getElementById("bt2").style.opacity = 1;
-		document.getElementById("bt3").style.opacity = 1;
 	},500);
 }
